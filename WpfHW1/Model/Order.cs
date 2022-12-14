2013350d10cf04a907a89d9939c8078d20acd33c
Максим Кротков <stalker-2204@mail.ru>
2022-12-14 21:22:03 +0300

    re

---
+++ WpfHW1/Model/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfHW1.Model
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Client { get; set; }
+        public List<Product> Products{ get; set; }
+        public decimal Price { get 
+            {
+                return Products.Sum(x => x.Price);
+            }
+        }
+
+    }
+}
